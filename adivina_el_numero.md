4c44461327daebb0159a6de4bdc0ec80da0d1a2c
Rei 1986 <Reigert_Lorigador@outlook.com>
2025-05-01 10:59:27 -0300

    se escribio el codigo principal del juego y se escribio un MD para que se tenga como referencia

---
+++ adivina_el_numero.md
@@ -0,0 +1,24 @@
+## JUEGO CREADOS EN PYTHON
+
+### Juego de adivinar el numero
+
+*El material en el cual me apoyo es:*
+**Inventa tus propios juegos de computadora con Python**
+**Tercera edicion**
+
+***
+Temas usados:
+
++ sentencias *import*
++ modulos 
++ sentencias *while*
++ condiciones
++ bloques
++ booleanos
++ operadores de comparacion
++ direfencia entre = y ==
++ sentencias *if*
++ la palabra reservada *break*
++ las funciones *str(), int() y float()*
++ la funcion *random.randint()*
+
--- main.py
+++ main.py
@@ -12,16 +12,31 @@
 	
 """
 import random
-print()
-def main():
-	#print("@@@@ Bienvendo al juego\n\tcomo te llamas?")
-	name = input()
-	#print(f"\tbueno, {name} estoy pensando en un numero entre 1 y 20")
-	#print(f"\tintenta adivinar")
-	guessN = int(input("introduce un numero"))
-	numberA = random.randint(1,20)
-	print(numberA)
-	print(guessN)
 
-	
-main()
+intentosRealizados = 0
+
+print("como te llamas?")
+miNombre = input()
+
+numero = random.randint(1,20)
+print(f"bueno {miNombre} estoy pensando en un numero entre 1 y 20.")
+
+while intentosRealizados < 6:
+    print("intenta adivinar")
+    estimacion = int(input())
+    intentosRealizados += 1
+    if estimacion < numero:
+        print("tu estimacion es muy baja.")
+    elif estimacion > numero:
+        print("tu estimacion es muy alta")
+    else:
+        print("adivinaste!!!")
+        break
+
+if estimacion == numero:
+    intentosRealizados = str(intentosRealizados)
+    print(f"buen trabajo {miNombre}! lo hiciste en {intentosRealizados} intentos")
+
+if estimacion != numero:
+    print(f"pues no. el numero que estaba pensando era {numero}")                 
+
