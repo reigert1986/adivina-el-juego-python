79cd34a0d91fb52182b1181672ed30784751ad24
Rei 1986 <Reigert_Lorigador@outlook.com>
2025-05-01 01:32:58 -0300

    revisar con log anterior

--- main.py
+++ main.py
@@ -14,16 +14,14 @@
 import random
 print()
 def main():
-	print("@@@@ Bienvendo al juego\n\tcomo te llamas?")
+	#print("@@@@ Bienvendo al juego\n\tcomo te llamas?")
 	name = input()
-	print(f"\tbueno, {name} estoy pensando en un numero entre 1 y 20")
-	print(f"\tintenta adivinar")
-	guessN = int(input())
-	numberA = random.randint(1,20+1)
-	for _ in range(25):
-         print(numberA)
- 	#print(numberA)
-	
+	#print(f"\tbueno, {name} estoy pensando en un numero entre 1 y 20")
+	#print(f"\tintenta adivinar")
+	guessN = int(input("introduce un numero"))
+	numberA = random.randint(1,20)
+	print(numberA)
+	print(guessN)
 
 	
 main()
