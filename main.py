efe00935d62698b34e026c8059115129a5c7817c
Rei 1986 <Reigert_Lorigador@outlook.com>
2025-05-01 00:51:17 -0300

    guardando mi primer commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+"""
+	INVENTA TU JUEGO CON PYTHON
+	1- JUEGO ADIVINA EL NUMERO.
+
+	la computadora pensará un número aleatorio entre 1 y 20
+		y te pedira que intentes adivinarlo
+
+
+	
+"""
+import random
+print()
+def main():
+	print("@@@@ Bienvendo al juego\n\tcomo te llamas?")
+	name = input()
+	print(f"\tbueno, {name} estoy pensando en un numero entre 1 y 20")
+	print(f"\tintenta adivinar")
+	guessN = int(input())
+	numberA = random.randint(1,20+1)
+	for _ in range(25):
+         print(numberA)
+ 	#print(numberA)
+	
+
+	
+main()
